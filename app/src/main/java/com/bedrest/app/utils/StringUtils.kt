6298c20e57f470fbf864e35c37aa73c8d7777e80
telkomdev-odiapratama <odiapratama@gmail.com>
2021-07-17 03:25:22 +0700

    Update custom maps Regex lat long check Rename list province

--- app/src/main/java/com/bedrest/app/utils/StringUtils.kt
+++ app/src/main/java/com/bedrest/app/utils/StringUtils.kt
@@ -12,7 +12,17 @@ object StringUtils {
 
     fun String.toKeywordPattern() = lowercase(Locale.getDefault()).replace(" ", "_")
 
-    fun Context.getStringArray(id: Int): Array<out String> {
+    fun Context.getStringArray(id: Int): Array<String> {
         return resources.getStringArray(id)
+    }
+
+    fun String.checkLatPattern(): Boolean {
+        val latPattern = Regex("^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)")
+        return matches(latPattern)
+    }
+
+    fun String.checkLonPattern(): Boolean {
+        val lonPattern = Regex("\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)\$")
+        return matches(lonPattern)
     }
 }
