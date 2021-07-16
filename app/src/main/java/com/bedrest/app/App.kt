04a3ce9b7fc14bfc397aa1b2cb5265a82382df80
telkomdev-odiapratama <odiapratama@gmail.com>
2021-07-16 09:21:02 +0700

    Fix load image vector

--- app/src/main/java/com/bedrest/app/App.kt
+++ app/src/main/java/com/bedrest/app/App.kt
@@ -4,4 +4,15 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+    }
+
+    companion object {
+        lateinit var INSTANCE: App
+            private set
+    }
+}
