54f067c802348cd9d45a7cb145ada97d681db4f9
Eldi Rohmanur Ikhsan <eldirohmanur@gmail.com>
2021-07-17 22:53:23 +0700

    Added marker click behavior and deleted FAB (#7)
    * Feature Map added
    
    * Added androidx-activity's viewModel inject
    
    * Changed hospital icon
    
    * Added provinces array resource
    
    * Added get array util
    
    * cleaning files
    
    * Integrate marker click
    
    * Feature Map added
    
    * cleaning files
    
    * fixing total bed string
    
    * Remove unused class
    
    * Change app icon
    
    * Change launcher background
    
    * Change icon check location
    
    * Update UI item availability
    
    * Add province suggestion
    
    * Update custom maps
    Regex lat long check
    Rename list province
    
    * Feature marker click behaviour added
    
    * Delete FAB
    
    * Fixing bottom sheet UI behavior
    
    * cleaning unused codes
    
    * Feature Map added
    
    * cleaning files
    
    * Feature Map added
    
    * cleaning files
    
    * Remove unused class
    
    * Feature marker click behaviour added
    
    * Delete FAB
    
    * Fixing bottom sheet UI behavior
    
    * cleaning unused codes
    
    * refactoring layout after rebasing
    
    Co-authored-by: telkomdev-odiapratama <odiapratama@gmail.com>

--- app/src/main/java/com/bedrest/app/base/activity/BaseActivity.kt
+++ app/src/main/java/com/bedrest/app/base/activity/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.bedrest.app.base.activity
 
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,9 @@ abstract class BaseActivity<V: ViewDataBinding>: AppCompatActivity(), BaseViewBi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
         initBinding(DataBindingUtil.setContentView(this, setLayout()), this)
         binding.lifecycleOwner = this
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
