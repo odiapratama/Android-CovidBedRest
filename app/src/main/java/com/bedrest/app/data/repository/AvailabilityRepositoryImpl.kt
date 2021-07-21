87e552f6a09ca48c39ac9f526add060e179a6f8a
rohmanureldi <eldirohmanur@gmail.com>
2021-07-21 16:04:36 +0700

    cleaning codes in repository

--- app/src/main/java/com/bedrest/app/data/repository/AvailabilityRepositoryImpl.kt
+++ app/src/main/java/com/bedrest/app/data/repository/AvailabilityRepositoryImpl.kt
@@ -2,12 +2,7 @@ package com.bedrest.app.data.repository
 
 import com.bedrest.app.data.model.AvailabilityItem
 import com.bedrest.app.data.model.BaseResponse
-import com.bedrest.app.domain.model.Availability
-import com.bedrest.app.data.model.ResultData
 import com.bedrest.app.data.remote.AvailabilityApi
-import com.bedrest.app.utils.ErrorUtils.getError
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AvailabilityRepositoryImpl @Inject constructor(
@@ -18,17 +13,6 @@ class AvailabilityRepositoryImpl @Inject constructor(
         province: String,
         revalidate: Boolean
     ): BaseResponse<List<AvailabilityItem>> {
-//        var result: ResultData<List<AvailabilityItem>>
-//        withContext(Dispatchers.IO) {
-//            result = try {
-//                val response = availabilityApi.getHospitalAvailability(province, revalidate)
-//                if (response.status == 200) ResultData.Success(response.data ?: emptyList())
-//                else ResultData.Error(response.error)
-//            } catch (e: Exception) {
-//                ResultData.Error(e.getError())
-//            }
-//        }
-//        return result
         return availabilityApi.getHospitalAvailability(province, revalidate)
     }
 }
